5d9ca73515eb7b6551aedd9158570f9b3e05472c
sasgas <sasgas@castis.com>
2016-10-25 19:05:01 +0900

    cryptopp i686 수정

--- install_cryptopp563.sh
+++ install_cryptopp563.sh
@@ -3,7 +3,7 @@ cd ~
 wget http://www.cryptopp.com/cryptopp563.zip
 unzip cryptopp563.zip -d cryptopp
 cd cryptopp
-sed -e s/march=native/march=x86-64/g GNUmakefile > tmp_make
+sed -e s/march=native/march=i686/g GNUmakefile > tmp_make
 mv tmp_make GNUmakefile
 make static
 make install
